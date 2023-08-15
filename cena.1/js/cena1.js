88fb66f9a740885af11e3bb8af0ce405e9be9dce
gabriele-sousa <gabriele.sousa14@etec.sp.gov.br>
2023-08-15 10:59:51 -0300

    atualizacao 2

--- cena.1/js/cena1.js
+++ cena.1/js/cena1.js
@@ -1,1 +1,23 @@
-Const
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+//cabeça
+ctx.beginPath();
+ctx.fillStyle="#54EC17";
+ctx.strokeStyle="#54EC17";
+ctx.arc(125, 180, 15, 0, Math.PI * 2);
+ctx.fill();
+ctx.stroke();
+
+//corpo
+ctx.beginPath();
+ctx.moveTo(100, 200);
+ctx.lineTo(125, 250);
+ctx.lineTo(150, 200);
+ctx.lineTo(100, 200);
+ctx.strokeStyle="#54EC17";
+ctx.fillStyle="#54EC17";
+ctx.stroke();
+ctx.fill();
